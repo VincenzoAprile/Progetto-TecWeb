c8eca22798598fcf9f96b4982b2b844e8019d665
Vincenzo Aprile <aprile.vincenzo202@gmail.com>
2026-06-28 10:56:45 +0200

    New Structure

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Progetto-TecWeb');
+  protected readonly title = signal('frontend');
 }
